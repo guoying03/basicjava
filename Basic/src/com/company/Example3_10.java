58f352667d3e1ee6d05fb6d3e07207610c03f06c
guoying03 <2834168063@qq.com>
2020-10-09 21:52:27 +0800

    10.9

--- Basic/src/com/company/Example3_10.java
+++ Basic/src/com/company/Example3_10.java
@@ -1,8 +1,34 @@
-package com.company;/**
-  *@ClassName Example3_10
-  *@Description TODO
-  *@Author GYY
-  *@Date 2020/10/9
-**/
+package com.company;
+
+/**
+ * @ClassName Example3_10
+ * @Description TODO
+ * @Author GYY
+ * @Date 2020/10/9
+ **/
 public class Example3_10 {
+    enum Friut{
+        苹果,梨,香蕉,西瓜,芒果
+    }
+    public static void main(String args[]){
+        double price = 0;
+        boolean show = false;
+        for(Friut firut:Friut.values()){
+            switch(firut){
+                case 苹果: price = 1.5;
+                show = true;
+                break;
+                case 芒果: price = 6.8;
+                show = true;
+                break;
+                case 香蕉: price = 2.8;
+                show = true;
+                break;
+                default:show = false;
+            }
+            if(show){
+                System.out.println( firut + "500克的价格：" + price+" 元 ");
+            }
+        }
+    }
 }
