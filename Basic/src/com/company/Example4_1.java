ceb7ab79d95d0c56a8c505595e3dc2e709c15d1e
guoying03 <2834168063@qq.com>
2020-10-11 18:50:32 +0800

    10.11

---
+++ Basic/src/com/company/Example4_1.java
@@ -0,0 +1,23 @@
+package com.company;
+
+/**
+ * @ClassName Example4_1
+ * @Description TODO
+ * @Author GYY
+ * @Date 2020/10/11
+ **/
+ class XiyoujiRenwu {
+    float height,weight;
+    String head,ear;
+    void speak(String s ){
+        System.out.println(s);
+
+    }
+}
+public class Example4_1{
+    public static void main(String args[]){
+        XiyoujiRenwu zhubajie,sunwukong;
+        zhubajie = new XiyoujiRenwu();
+        sunwukong = new XiyoujiRenwu();
+    }
+}
